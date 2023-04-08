7450f0fd6ec5aeacac529f529d472ce4895288fa
Oqil Ahmad <oqilaxmatov24@gmail.com>
2023-04-08 23:44:18 +0500

    initialize npm package

---
+++ src/index.js
@@ -0,0 +1,62 @@
+/**
+ * Password generator library
+ */
+class PasswordGenerator {
+  /**
+   * Creates a new PasswordGenerator instance.
+   * @param {Object} options - The options to configure the password generator.
+   * @param {Number} options.length - The length of the generated password.
+   * @param {Boolean} options.includeUppercase - Whether to include uppercase letters in the generated password.
+   * @param {Boolean} options.includeLowercase - Whether to include lowercase letters in the generated password.
+   * @param {Boolean} options.includeNumbers - Whether to include numbers in the generated password.
+   * @param {Boolean} options.includeSymbols - Whether to include symbols in the generated password.
+   */
+  constructor(options) {
+    const defaultOptions = {
+      length: 12,
+      includeUppercase: true,
+      includeLowercase: true,
+      includeNumbers: true,
+      includeSymbols: true,
+    };
+    this.options = Object.assign({}, defaultOptions, options);
+  }
+
+  /**
+   * Generates a random password.
+   * @returns {String} The generated password.
+   */
+  generate() {
+    let password = "";
+    const charset = this._getCharset();
+    for (let i = 0; i < this.options.length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+    }
+    return password;
+  }
+
+  /**
+   * Returns the character set based on the current options.
+   * @returns {String} The character set to use for generating passwords.
+   * @private
+   */
+  _getCharset() {
+    let charset = "";
+    if (this.options.includeUppercase) {
+      charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if (this.options.includeLowercase) {
+      charset += "abcdefghijklmnopqrstuvwxyz";
+    }
+    if (this.options.includeNumbers) {
+      charset += "0123456789";
+    }
+    if (this.options.includeSymbols) {
+      charset += "!@#$%^&*()_+-=[]{}|;:,.<>?";
+    }
+    return charset;
+  }
+}
+
+module.exports = PasswordGenerator;
